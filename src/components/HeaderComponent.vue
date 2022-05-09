1acd787f0ac37a2fba5315d5c8adcd087ae59d5d
gianmarco <gianmarco.culqui.ikaros@gmail.com>
2022-05-09 15:44:19 +0200

    up

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+<header class="nav">
+   
+</header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.nav{
+  height: 100px;
+  width: 100%;
+  background-color: #2e3a46;
+  color: white;
+
+}
+
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+<main>
+    fgdknj
+</main>
+</template>
+
+<script>
+export default {
+    name: 'MainComponent',
+    
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    background-color: #1e2d3b;
+    width: 100%;
+}
+
+</style>
